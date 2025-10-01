fa066e43b50dbf5779137dbfbe9659ecd09d2fa5
yamkaz <yk.kazuyuki@gmail.com>
2025-10-01 11:48:46 +0900

    🔧 Tailwind と HeroUI セットアップ調整

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
 import type { Config } from "tailwindcss";
-import { heroui } from "@heroui/theme";
+import { heroui } from "@heroui/react";
 
 const config: Config = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
     extend: {},
   },
